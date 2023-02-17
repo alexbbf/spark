d3042b719826a167f48ba6dce6fbb83e57634d02
alexbbf <alex.bbf@hotmail.com>
2023-02-17 11:30:58 -0400

    validação servico prestado

--- src/main/java/io/github/alexbbf/spark/rest/dto/ServicoPrestadoDTO.java
+++ src/main/java/io/github/alexbbf/spark/rest/dto/ServicoPrestadoDTO.java
@@ -3,12 +3,21 @@ package io.github.alexbbf.spark.rest.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 public class ServicoPrestadoDTO {
 
+    @NotNull(message = "{campo.descricao.obrigatorio}")
     private String descricao;
+
+    @NotNull(message = "{campo.preco.obrigatorio}")
     private String preco;
+
+    @NotNull(message = "{campo.data.obrigatorio}")
     private String data;
+
+    @NotNull(message = "{campo.pessoa.obrigatorio}")
     private Integer idPessoa;
 }
